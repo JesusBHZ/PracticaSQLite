85dbdb58ac9e4dbca1c8679c18ea24e441c44a22
Jesus Bautista <97202824+JesusBHZ@users.noreply.github.com>
2022-05-11 16:52:47 +0000

    Avanse 1

--- main.sql
+++ main.sql
@@ -1,6 +1,6 @@
 -- Activa los encabezados
 -- Modo: Columna
--- Actia uso de llaves foraneas
+-- Activa uso de llaves foraneas
 .headers on
 .mode column
 PRAGMA foreign_keys = ON;
--- tablas/index.sql
+++ tablas/index.sql
@@ -1,8 +1,5 @@
-/*INSERT INTO clientes(nombre,email)
-VALUES('Dejah','otro@email.com');
-
-INSERT INTO clientes(nombre,email)
-VALUES('Otro','dejah@email.com');
-
-INSERT INTO clientes(nombre,email)
-VALUES('Dejah','dejah@email.com');*/
+/*INSERT INTO ventas(id_venta,fecha,id_cliente)
+VALUES(3,'11/05/2022',3);*/
+INSERT INTO detalle_ventas(id_venta,id_producto,cantidad_producto,precio_unitario,total_x_producto)
+VALUES
+(3,1,2,10,20);
