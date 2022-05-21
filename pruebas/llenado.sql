2be9e3335e2b4d1fea59d19346e38872a719790e
Jesus Bautista <97202824+JesusBHZ@users.noreply.github.com>
2022-05-21 20:22:05 +0000

    1

--- pruebas/llenado.sql
+++ pruebas/llenado.sql
@@ -1,1 +1,1 @@
-insert into productos(id_producto,producto,categoria,precio_unitario,existencias,unidad) select id_producto,producto,categoria,precio_unitario,existencias,unidad from temp; 
+insert into productos(id_producto,producto,categoria,precio_unitario,existencias,unidad) select id_producto,producto,categoria,precio_unitario,existencias,unidad from tempo;
